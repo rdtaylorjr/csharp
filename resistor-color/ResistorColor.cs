50dceccfd76a002de82648efecdf9a3bd497b78d
Russell Taylor <rdtaylorjr@protonmail.com>
2021-01-21 21:53:26 -0600

    Create ResistorColor.cs

---
+++ resistor-color/ResistorColor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    public static int ColorCode(string color)
+    {
+        for (int i = 0; i < Colors().Length - 1; ++i)
+        {
+            if (color == Colors()[i])
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    public static string[] Colors() => new[]
+        { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+}
