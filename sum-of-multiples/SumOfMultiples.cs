7a297efc5f2dab9c80c7b4f833212c22a4225a06
Russell Taylor <rdtaylorjr@protonmail.com>
2021-03-08 21:37:54 -0600

    Create sum-of-multiples
    Solution to the exercism.io csharp sum-of-multiples problem

---
+++ sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        for (int i = 0; i < max; ++i)
+        {
+            for (var j in multiples)
+            {
+                
+            }
+        }
+    }
+}
